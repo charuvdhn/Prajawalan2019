73ea26d49389456ab44a16d4a48ca3ae466a6313
charuvdhn <40365154+charuvdhn@users.noreply.github.com>
2019-02-09 02:08:16 +0530

    Update events.php

--- events.php
+++ events.php
@@ -47,15 +47,419 @@
 			include './common/header/header.php' ;
 		?>
         
-        <div style="
-    text-align: center;
+<div id="top"></div>
+
+<div class="container">
+
+
+<!-- BF PROMO -->
+<script type="text/javascript" src="/bf5.js"></script>
+<!-- BF PROMO -->
+<!-- /header --> 
+
+<div>
+        <!-- Start WOWSlider.com BODY section -->
+        
+
+
+<div id="wowslider-container1">
+<div class="ws_images"><ul>
+<li><img src="images/an.jpg" alt="Bernese Oberland - Switzerland jquery image gallery " title="Andromaster" id="wows1_0"/></li>
+<li><img src="images/ag.jpg" alt="bootstrap jquery lightbox" title="Angry Birds" id="wows1_1"/></a></li>
+<li><img src="images/bc.jpg" alt="Mountain landscape  - Switzerland image gallery jquery " title="Box Cricket" id="wows1_2"/></li>
+<li><img src="images/4.jpg" alt="Lauterbrunnen - Switzerland jquery gallery " title="CAD Strife" id="wows1_3"/></li>
+<li><img src="images/5.jpg" alt="Lucerne - Switzerland jquery gallery slider " title="Chanakya" id="wows1_4"/></li>
+<li><img src="images/6.jpg" alt="View from the Schilthorn mountain - Switzerland gallery slider jquery " title="Circuit Mania" id="wows1_5"/></li>
+<li><img src="images/7.jpg" alt="Schilthorn - Switzerland jquery slider gallery " title="Encipher" id="wows1_6"/></li>
+<li><img src="images/g.png" alt="Zurich - Switzerland jquery image gallery slider " title="Googler" id="wows1_7"/></li>
+<li><img src="images/9.jpg" alt="Zurich - Switzerland jquery image gallery slider " title="LAN Gaming" id="wows1_7"/></li>
+<li><img src="images/m.jpg" alt="Zurich - Switzerland jquery image gallery slider " title="Mastermind" id="wows1_7"/></li>
+<li><img src="images/11.jpg" alt="Zurich - Switzerland jquery image gallery slider " title="Mod Expo Group(A,b,C,D)" id="wows1_7"/></li>
+<li><img src="images/mh.jpg" alt="Zurich - Switzerland jquery image gallery slider " title="Mystery Hunt" id="wows1_7"/></li>
+<li><img src="images/13.jpg" alt="Zurich - Switzerland jquery image gallery slider " title="Paper Presentation Group(A,b,C,D)" id="wows1_7"/></li>
+<li><img src="images/r.jpg" alt="Zurich - Switzerland jquery image gallery slider " title="Robotics" id="wows1_7"/></li>
+<li><img src="images/rb.jpg" alt="Zurich - Switzerland jquery image gallery slider " title="Rubik Cube" id="wows1_7"/></li>
+<li><img src="images/16.jpg" alt="Zurich - Switzerland jquery image gallery slider " title="Sci-Lab" id="wows1_7"/></li>
+<li><img src="images/bb.jpg" alt="Zurich - Switzerland jquery image gallery slider " title="Bridge Building" id="wows1_7"/></li>
+<li><img src="images/18.jpg" alt="Zurich - Switzerland jquery image gallery slider " title="Virtual Placement" id="wows1_7"/></li>
+<li><img src="images/we.jpg" alt="Zurich - Switzerland jquery image gallery slider " title="Webeinstein" id="wows1_7"/></li>
+<li><img src="images/sc.jpg" alt="Zurich - Switzerland jquery image gallery slider " title="Smart City Planning" id="wows1_7"/></li>
+<li><img src="images/lt.jpg" alt="Zurich - Switzerland jquery image gallery slider " title="Lets Talk" id="wows1_7"/></li>
+
+</ul></div>
+<div class="ws_thumbs">
+<div>
+<a href="#" title="Andromaster"><img src="images/1_thumb.jpg" alt="slider gallery jquery 
+" /> </a>
+<a href="#" title="Angry Bird"><img src="images/2_thumb.jpg" alt="jquery gallery plugin 
+" /> </a>
+<a href="#" title="Box Cricket"><img src="images/3_tn.jpg" alt="jquery gallery slideshow 
+" /> </a>
+<a href="#" title="Cad Strife"><img src="images/4_tn.jpg" alt="jquery image gallery plugin 
+" /> </a>
+<a href="#" title="Chanakya"><img src="images/5_tn.jpg" alt="jquery gallery free 
+" /> </a>
+<a href="#" title="Circuit Mania"><img src="images/6_tn.jpg" alt="free image gallery jquery 
+" /> </a>
+<a href="#" title="Encipher"><img src="images/7_tn.jpg" alt="jquery image gallery free 
+" /> </a>
+<a href="#" title="Googler"><img src="images/8_tn.jpg" alt="gallery plugin jquery 
+" /> </a>
+
+<a href="#" title="LAN Gaming"><img src="images/9_tn.jpg" alt="gallery plugin jquery 
+" /> </a>
+
+<a href="#" title="Mastermind"><img src="images/10_tn.jpg" alt="gallery plugin jquery 
+" /> </a>
+
+<a href="#" title="Mod Expo Group(A,b,C,D)"><img src="images/11_tn.jpg" alt="gallery plugin jquery 
+" /> </a>
+
+<a href="#" title="Mystery Hunt"><img src="images/12_tn.jpg" alt="gallery plugin jquery 
+" /> </a>
+
+<a href="#" title="Paper Presentation Group(A,b,C,D)"><img src="images/13_tn.jpg" alt="gallery plugin jquery 
+" /> </a>
+
+<a href="#" title="Robotics"><img src="images/14_tn.jpg" alt="gallery plugin jquery 
+" /> </a>
+
+<a href="#" title="Rubik Cube"><img src="images/15_tn.jpg" alt="gallery plugin jquery 
+" /> </a>
+
+<a href="#" title="Sci-Lab"><img src="images/16_tn.jpg" alt="gallery plugin jquery 
+" /> </a>
+
+<a href="#" title="Bridge Building"><img src="images/bb.jpg" alt="gallery plugin jquery 
+" /> </a>
+
+<a href="#" title="Virtual Placement"><img src="images/18_tn.jpg" alt="gallery plugin jquery 
+" /> </a>
+
+<a href="#" title="Webeinstein"><img src="images/19_tn.jpg" alt="gallery plugin jquery 
+" /> </a>
+
+<a href="#" title="Smart City Planning"><img src="images/sc.jpg" alt="gallery plugin jquery 
+" /> </a>
+
+<a href="#" title="Lets Talk"><img src="images/lt.jpg" alt="gallery plugin jquery 
+" /> </a>
+
+</div>
+<script type="text/javascript" src="http://wowslider.com/images/demo/wowslider.js"></script>
+<script type="text/javascript" src="http://wowslider.com/sliders/demo-65/engine1/script.js"></script>
+        <!-- End WOWSlider.com BODY section -->
+<br />
+</div>
+<style>
+  @import url(https://fonts.googleapis.com/css?family=Marck+Script&subset=latin,cyrillic,latin-ext);
+#wowslider-container1 { 
+  zoom: 1; 
+  position: relative; 
+  max-width:740px;
+  margin:0 40px 0 20px;
+  z-index:90;
+  border:3px solid #FFFFFF;
+  text-align:left; /* reset align=center */
+  font-size: 10px;
+}
+* html #wowslider-container1{ width:740px }
+#wowslider-container1 .ws_images ul{
+  position:relative;
+  width: auto; 
+  height:auto;
+  left:0;
+  list-style:none;
+  margin:0;
+  padding:0;
+  border-spacing:0;
+  overflow: visible;
+  /*table-layout:fixed;*/
+}
+#wowslider-container1 .ws_images ul li{
+  width:auto;
+  line-height:0; /*opera*/
+  float:left;
+  font-size:0;
+  padding:0 0 0 0 !important;
+  margin:0 0 0 0 !important;
+}
+
+#wowslider-container1 .ws_images{
+  position: relative;
+  left:0;
+  top:0;
+  width:100%;
+  height:100%;
+  overflow:hidden;
+}
+#wowslider-container1 .ws_images a{
+  width:100%;
+  display:block;
+  color:transparent;
+}
+#wowslider-container1 img{
+  max-width: none !important;
+}
+#wowslider-container1 .ws_images img{
+  width:100%;
+  border:none 0;
+  max-width: none;
+  padding:0;
+  margin:0;
+}
+#wowslider-container1 a{ 
+  text-decoration: none; 
+  outline: none; 
+  border: none; 
+}
+
+#wowslider-container1  .ws_bullets { 
+  font-size: 0px; 
+  float: left;
+  position:absolute;
+  z-index:70;
+}
+#wowslider-container1  .ws_bullets div{
+  position:relative;
+  float:left;
+}
+#wowslider-container1  .wsl{
+  display:none;
+}
+#wowslider-container1 sound, 
+#wowslider-container1 object{
+  position:absolute;
+}
+
+#wowslider-container1 .ws_photoItem {
+  border: 2em solid #fff;
+  margin-left: -2em;
+  margin-top: -2em;
+}
+
+
+#wowslider-container1  .ws_bullets { 
+  padding: 4px; 
+}
+#wowslider-container1 .ws_bullets a { 
+  width:15px;
+  height:15px;
+  background: url(./bullet.png) left top;
+  float: left; 
+  text-indent: -4000px; 
+  position:relative;
+  margin-left:5px;
+  color:transparent;
+}
+#wowslider-container1 .ws_bullets a:hover, #wowslider-container1 .ws_bullets a.ws_selbull{
+  background-position: 0 100%;
+}
+#wowslider-container1 a.ws_next, #wowslider-container1 a.ws_prev {
+  position:absolute;
+  display:none;
+  top:50%;
+  margin-top:-1.6em;
+  z-index:60;
+  width: 2.4em; 
+  height: 3.2em;
+  background-image: url(./arrows.png);
+  background-size:200%;
+}
+#wowslider-container1 a.ws_next{
+  background-position: 100% 0;
+  right:3px;
+}
+#wowslider-container1 a.ws_prev {
+  background-position: 0 0; 
+  left:3px;
+}
+* html #wowslider-container1 a.ws_next,* html #wowslider-container1 a.ws_prev{display:block}
+#wowslider-container1:hover a.ws_next, #wowslider-container1:hover a.ws_prev {display:block}
+
+/*playpause*/
+#wowslider-container1 .ws_playpause {
+  display:none;
+    width: 2.4em;
+    height: 3.2em;
+    position: absolute;
     top: 50%;
-    left: 0;
-    position: fixed;
-    right: 0;">
-            <h3 class="eve">Events are coming soon.....</h3>
-        </div>
-		
+    left: 50%;
+    margin-left: -1.2em;
+    margin-top: -1.6em;
+    z-index: 59;
+  background-size:100%;
+}
+
+#wowslider-container1:hover .ws_playpause {
+  display:block;
+}
+
+#wowslider-container1 .ws_pause {
+    background-image: url(./pause.png);
+}
+
+#wowslider-container1 .ws_play {
+    background-image: url(./play.png);
+}
+
+#wowslider-container1 .ws_pause:hover, #wowslider-container1 .ws_play:hover {
+    background-position: 100% 100% !important;
+}/* bottom center */
+#wowslider-container1  .ws_bullets {
+    bottom: 0px;
+  left:50%;
+}
+#wowslider-container1  .ws_bullets div{
+  left:-50%;
+}
+#wowslider-container1 .ws-title{
+  position:absolute;
+  display:block;
+  bottom: 2em;
+  left: 0;
+  margin-right: 1em; 
+  padding:1em;
+  background:#FFFFFF;
+  color:#555555;
+  z-index: 50;
+  font-family:'Marck Script',"Comic Sans MS",cursive;
+  line-height: 2.2em;
+  font-weight: normal;
+  text-shadow: none;
+}
+#wowslider-container1 .ws-title div{
+  margin-top:0.15em;
+  font-size: 2em;
+  line-height: 1.4em;
+}
+#wowslider-container1 .ws-title span{
+  font-size: 2.8em;
+}#wowslider-container1  .ws_thumbs { 
+  font-size: 0px; 
+  position:absolute;
+  overflow:auto;
+  z-index:70;
+}
+#wowslider-container1 .ws_thumbs img{
+  text-decoration: none;
+  border: 0;
+  width: 100%;
+}
+#wowslider-container1 .ws_thumbs a {
+  position:relative;
+  text-indent: -4000px; 
+  color:transparent;
+  opacity:0.85;
+  text-decoration: none;
+  display: inline-block;
+  border: 0;
+  margin:1.51%;
+  text-indent:0;
+  padding: 2.01%;
+  width: 92.96%;
+}
+#wowslider-container1 .ws_thumbs a:hover{
+  opacity:1;
+}
+#wowslider-container1 .ws_thumbs a:hover img{
+  visibility:visible;
+}
+#wowslider-container1  .ws_thumbs { 
+    right: -27.57%;
+    top: 0;
+  width:26.89%;
+  height:100%;
+}
+#wowslider-container1  .ws_thumbs div{
+  position:relative;
+  width:100%;
+}#wowslider-container1 .ws_thumbs a.ws_selthumb{
+  background-color:#FFFFFF;
+    box-shadow: 0px 0px 3px rgba(100, 100, 100, 0.7); 
+    -moz-box-shadow: 0px 0px 3px rgba(100, 100, 100, 0.7);
+    -webkit-box-shadow: 0px 0px 3px rgba(100, 100, 100, 0.7);
+}
+
+#wowslider-container1 .ws_thumbs  a{
+    background-color:#FFFFFF;
+}
+
+#wowslider-container1 .ws_images > ul{
+  animation: wsBasic 48s infinite;
+  -moz-animation: wsBasic 48s infinite;
+  -webkit-animation: wsBasic 48s infinite;
+}
+@keyframes wsBasic{0%{left:-0%} 8.33%{left:-0%} 12.5%{left:-100%} 20.83%{left:-100%} 25%{left:-200%} 33.33%{left:-200%} 37.5%{left:-300%} 45.83%{left:-300%} 50%{left:-400%} 58.33%{left:-400%} 62.5%{left:-500%} 70.83%{left:-500%} 75%{left:-600%} 83.33%{left:-600%} 87.5%{left:-700%} 95.83%{left:-700%} }
+@-moz-keyframes wsBasic{0%{left:-0%} 8.33%{left:-0%} 12.5%{left:-100%} 20.83%{left:-100%} 25%{left:-200%} 33.33%{left:-200%} 37.5%{left:-300%} 45.83%{left:-300%} 50%{left:-400%} 58.33%{left:-400%} 62.5%{left:-500%} 70.83%{left:-500%} 75%{left:-600%} 83.33%{left:-600%} 87.5%{left:-700%} 95.83%{left:-700%} }
+@-webkit-keyframes wsBasic{0%{left:-0%} 8.33%{left:-0%} 12.5%{left:-100%} 20.83%{left:-100%} 25%{left:-200%} 33.33%{left:-200%} 37.5%{left:-300%} 45.83%{left:-300%} 50%{left:-400%} 58.33%{left:-400%} 62.5%{left:-500%} 70.83%{left:-500%} 75%{left:-600%} 83.33%{left:-600%} 87.5%{left:-700%} 95.83%{left:-700%} }
+
+#wowslider-container1 {
+    box-shadow: 0px 0px 3px rgba(100, 100, 100, 0.7); 
+    -moz-box-shadow: 0px 0px 3px rgba(100, 100, 100, 0.7);
+    -webkit-box-shadow: 0px 0px 3px rgba(100, 100, 100, 0.7); 
+}
+#wowslider-container1 .ws_bullets  a img{
+  text-indent:0;
+  display:block;
+  bottom:20px;
+  left:-93px;
+  visibility:hidden;
+  position:absolute;
+    border: 4px solid #FFFFFF;
+    box-shadow: 0px 0px 3px rgba(100, 100, 100, 0.7); 
+    -moz-box-shadow: 0px 0px 3px rgba(100, 100, 100, 0.7);
+    -webkit-box-shadow: 0px 0px 3px rgba(100, 100, 100, 0.7); 
+  max-width:none;
+}
+#wowslider-container1 .ws_bullets a:hover img{
+  visibility:visible;
+}
+
+#wowslider-container1 .ws_bulframe div div{
+  height:90px;
+  overflow:visible;
+  position:relative;
+}
+#wowslider-container1 .ws_bulframe div {
+  left:0;
+  overflow:hidden;
+  position:relative;
+  width:185px;
+  background-color:#FFFFFF;
+}
+#wowslider-container1  .ws_bullets .ws_bulframe{
+  display:none;
+  bottom:20px;
+  overflow:visible;
+  position:absolute;
+  cursor:pointer;
+    border: 4px solid #ffffff;
+    box-shadow: 0px 0px 3px rgba(100, 100, 100, 0.7); 
+    -moz-box-shadow: 0px 0px 3px rgba(100, 100, 100, 0.7);
+    -webkit-box-shadow: 0px 0px 3px rgba(100, 100, 100, 0.7);
+}
+#wowslider-container1 .ws_bulframe span{
+  display:block;
+  position:absolute;
+  bottom:-12px;
+  margin-left:-4px;
+  left:93px;
+  background:url(./triangle.png);
+  width:15px;
+  height:8px;
+}
+#wowslider-container1 {
+  cursor: -webkit-grab;
+  cursor: -moz-grab;
+  cursor: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAQAAADZc7J/AAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAABwSURBVEjH7ZJBEsAgCAMT/v/n9NCOSqe2oD2yNx1JggB4BCEFWyFASP2KMQE7ywWhe/tTRGCGogLk02tFctiW/SUgaMyQG4PdPzDn31rQbMb8FiAXgvsEJNax1yVlVGAjA93apP3HFhZTGIqiKH7iADB6HxPlHdNVAAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDE0LTA3LTA3VDEzOjQ5OjEwKzAyOjAwm7WiFAAAACV0RVh0ZGF0ZTptb2RpZnkAMjAxNC0wNy0wN1QxMzo0OToxMCswMjowMOroGqgAAAAASUVORK5CYII="), move;
+}
+#wowslider-container1.grabbing {
+  cursor: -webkit-grabbing;
+  cursor: -moz-grabbing;
+  cursor: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAQAAADZc7J/AAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAABaSURBVEjH7ZMxCkAhDEOT8u9/5TpJ+xWkFse8IYutJgEB8RCHL1qCc90BEFnT6QH7mwgFHBUf8wJyS1TDLuc3vmighx37LZdIth3E5hKj9n6O0HRh+oJCiFcMxRUUDxR1CTMAAAAldEVYdGRhdGU6Y3JlYXRlADIwMTQtMDctMDdUMTM6NDk6MzgrMDI6MDDqf+sOAAAAJXRFWHRkYXRlOm1vZGlmeQAyMDE0LTA3LTA3VDEzOjQ5OjM4KzAyOjAwmyJTsgAAAABJRU5ErkJggg=="), move;
+}
+</style>
 		
 		<script src="./common/background/background.js"></script>
 		
